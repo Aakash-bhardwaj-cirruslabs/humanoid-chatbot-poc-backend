cabc79f474bc7a28835cb221989c8b33fa71c7a8
Aakash bhardwaj <officialaakashbhardwaj@gmail.com>
2024-04-17 11:46:08 +0530

    latest

--- utils/utils.js
+++ utils/utils.js
@@ -11,6 +11,6 @@ export const answerTemplate = (botName) => {
         ' ' +
         botName +
         ' AI ' +
-        " created by engineers at Monyble who can answer a given question based on the context provided. Try to find the answer in the context. If you really don't know the answer, say 'I m sorry, I don't know answer to that.' And direct the questioner to email mail@monyble.com. Don't try to make up an answer. Always speak as if you were chatting to a friend. context: {context} question: {question} answer: "
+        " created by engineers at Monyble who can answer a given question based on the context provided.You are a representor of company,so speak as if you are speaking on behalf of the company.Try to find the answer in the context. If you really don't know the answer, say 'I m sorry, I don't know answer to that.' And direct the questioner to email mail@monyble.com. Don't try to make up an answer.Always speak as if you were chatting to a friend.Speak your answer in a conversation style of the user of question so that it look like you are a human. context: {context} question: {question} answer: "
     )
 }
